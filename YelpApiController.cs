37cad5eb7f01d9d8f75671e0c5c68a77969cbf58
Ian Cha <chaheein@gmail.com>
2020-07-12 05:47:40 -0700

    done

--- YelpApiController.cs
+++ YelpApiController.cs
@@ -2,9 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using ichoose_api.Models;
-//using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Extensions.Hosting;
 
 namespace ichoose_api
 {
@@ -18,42 +16,28 @@ namespace ichoose_api
     public YelpApiController(IHttpClientFactory clientFactory)
     {
       _clientFactory = clientFactory;
-
     }
 
     [HttpPost]
     public async Task<ActionResult> YelpApiAsync([FromBody] ResultData resultData)
     {
+      YelpResponse yelpResponse;
 
-      Console.WriteLine(resultData.latitude);
-      Console.WriteLine(resultData.longitude);
-      Console.WriteLine(resultData.searchKey);
-      var request = new HttpRequestMessage(HttpMethod.Get, "https://api.yelp.com/v3/businesses/search?term=korean&location=milpitas");
-      request.Headers.Add("Authorization", "Bearer DqqvJj3BbphPQC61RYWyhK3WbmKfKi-s0sFkT9goODs3_2HYuuvI-_-eZ6PMM-eYcT2hXDCCDOFfivF7UKgfeMESjH6P4iDcGyIxekBwzSB6Aw2rHOs0zbBg4L-iXXYx");
-      var client = _clientFactory.CreateClient();
+      var request = new HttpRequestMessage(
+        HttpMethod.Get,
+        $"?term={resultData.searchKey}&latitude={resultData.latitude}&longitude={resultData.longitude}"
+      );
+      var client = _clientFactory.CreateClient("yelp");
       var response = await client.SendAsync(request);
 
-
-
-
-
-      //HttpResponseMessage response = await client.SendAsync(request);
-
-
-      Console.WriteLine(response);
-
-
-
-
-
-
-
-
-
+      if (response.IsSuccessStatusCode)
+      {
+        yelpResponse = await response.Content.ReadAsAsync<YelpResponse>();
+        //Console.WriteLine(yelpResponse.name, " ha");
+        Console.WriteLine(yelpResponse.businesses.Count + " yoyoyo");
+        return Ok(yelpResponse.businesses);
+      }
       return Ok("www");
     }
   }
-
-
-
 }
